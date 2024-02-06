d427ed14318c8977db0d4f4d1bd54f57e7121dfa
vittal <vitthalsshindhe2001@gmail.com>
2024-02-06 17:40:48 +0530

    ui

---
+++ app/component/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello World !</h1>
+      <h1>Hello World !</h1>
+      <h1>Hello World !</h1>
+      <h1>Hello World !</h1>
+    </div>
+  )
+}
---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import App from './component/App'
+import About from './component/About'
+export default function page() {
+  return (
+    <>
+      <App />
+      <About />
+    </>
+  )
+}
